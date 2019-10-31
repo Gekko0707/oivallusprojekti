eef2a3122b49f31a6dc14cc8eec8e09de0d27097
Gekko0707 <57213520+Gekko0707@users.noreply.github.com>
2019-10-31 12:12:46 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Perjantai 3110</title>
+    <link rel="stylesheet" href="styles.css"/>
+</head>
+<body>
+    <h1>Mitä en oppinut tänäänkään?</h1>
+
+    <ul>
+        <li><a href="default.asp">Home</a></li>
+        <li><a href="news.asp">News</a></li>
+        <li><a href="contact.asp">Contact</a></li>
+        <li><a href="about.asp">About</a></li>
+      </ul>
+
+    <table style="width:100%">
+    <tr>
+        <th>päivä</th>
+        <th>oivallus</th>
+        <th>poista</th>
+    </tr>
+    <tr>
+        <td>31.10.19</td>
+        <td>Marvelia EI käytetä</td>
+        <td>X</td>
+    </tr>
+    <tr>
+        <td>01.11.19</td>
+        <td>hammaslääkäri</td>
+        <td></td>
+    </tr>
+    </table>
+
+</body>
+</html>
